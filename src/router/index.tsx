f2fcfc15b187eaeb2cee749a013dd791bd3f109f
jenny <jenny@jennydeMacBook-Pro.local>
2025-04-07 12:29:38 +0800

    sw

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import PhoneInput from "../components/PhoneInput";
 import Login from "../components/Login";
 import Validate from '../components/ValiPage'
 import NewLogin from "../components/NewLogin";
 import CodePage from '../components/CodePage'
 import Health from '../components/Health'
-import BeforeLogin from '../components/BeforeLogin' 
+import BeforeLogin from '../components/BeforeLogin' // Removing since module not found
 
 // 模拟是否登录
 const isAuthenticated = false;
 
 const AppRouter: React.FC = () => {
   return (
-    <Router>
+    <Router basename="/qq2">
       <Routes>
         <Route path="/" element={<PhoneInput />} />
         <Route
